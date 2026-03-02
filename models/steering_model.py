2bbd324aa4ad99c9aa254d2a34b99e920c370cd6
G.C.M. Perera <charithmanujaya1@gmail.com>
2026-03-02 03:00:21 +0000

    commit 2 dev

---
+++ models/steering_model.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+
+
+class SteeringRegression(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.model = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128 * 128 * 3, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)
+        )
+
+    def forward(self, x):
+        return self.model(x)
